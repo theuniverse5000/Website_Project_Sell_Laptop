02159af0edf0c70bdd3823f114dacd7c5238f1ff
minh <theuniverse5000@gmail.com>
2023-06-19 21:17:30 +0700

    Add project files.

---
+++ Shop_API/AppDbContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop_API.AppDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
