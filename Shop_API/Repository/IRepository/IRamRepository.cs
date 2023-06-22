c4610518d1bbbf1b17a796644816720af2fdac43
minh <theuniverse5000@gmail.com>
2023-06-22 22:02:55 +0700

    Ram Repository

---
+++ Shop_API/Repository/IRepository/IRamRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shop_Models.Entities;
+
+namespace Shop_API.Repository.IRepository
+{
+    public interface IRamRepository
+    {
+        Task<bool> Create(Ram obj);
+        Task<bool> Update(Ram obj);
+        Task<bool> Delete(Guid id);
+        Task<List<Ram>> GetAllRams();
+    }
+}
---
+++ Shop_API/Repository/RamRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop_API.AppDbContext;
+using Shop_API.Repository.IRepository;
+using Shop_Models.Entities;
+
+namespace Shop_API.Repository
+{
+    public class RamRepository : IRamRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public RamRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Create(Ram obj)
+        {
+            var checkMa = await _context.Rams.AnyAsync(x => x.Ma == obj.Ma);// tìm mã, trả về true nếu đã có, false nếu chưa có
+            if (obj == null || checkMa == true)
+            {
+                return false;
+            }
+            try
+            {
+                await _context.Rams.AddAsync(obj);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public async Task<bool> Delete(Guid id)
+        {
+            var ram = await _context.Rams.FindAsync(id);
+            if (ram == null)
+            {
+                return false;
+            }
+            try
+            {
+                ram.TrangThai = 0;
+                _context.Rams.Update(ram);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public async Task<List<Ram>> GetAllRams()
+        {
+            var list = await _context.Rams.ToListAsync();// lấy tất cả ram
+            var listRam = list.Where(x => x.TrangThai != 0).ToList();// lấy tất cả ram với điều kiện trạng thái khác 0
+            return listRam;
+        }
+        public async Task<bool> Update(Ram obj)
+        {
+            var ram = await _context.Rams.FindAsync(obj.Id);
+            if (ram == null)
+            {
+                return false;
+            }
+            try
+            {
+                ram.ThongSo = obj.ThongSo;
+                ram.TrangThai = obj.TrangThai;
+                _context.Rams.Update(ram);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
