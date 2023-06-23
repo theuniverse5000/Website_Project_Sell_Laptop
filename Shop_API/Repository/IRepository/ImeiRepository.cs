e2e334d5ca63c687b0badb047f3d6c0fa0234f82
minh <theuniverse5000@gmail.com>
2023-06-23 17:46:51 +0700

    Imei Repository

---
+++ Shop_API/Repository/IRepository/ImeiRepository.cs
@@ -0,0 +1,27 @@
+﻿using Shop_Models.Entities;
+
+namespace Shop_API.Repository.IRepository
+{
+    public class ImeiRepository : IImeiRepository
+    {
+        public Task<bool> Create(Imei obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Screen>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Imei obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
