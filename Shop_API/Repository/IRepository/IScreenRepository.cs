748b1c444d6a9a8fd9e963338e6e4dfe26a45e32
minh <theuniverse5000@gmail.com>
2023-06-23 12:59:17 +0700

    Screen Repository, Screen API

---
+++ Shop_API/Repository/IRepository/IScreenRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shop_Models.Entities;
+
+namespace Shop_API.Repository.IRepository
+{
+    public interface IScreenRepository
+    {
+        Task<bool> Create(Screen obj);
+        Task<bool> Update(Screen obj);
+        Task<bool> Delete(Guid id);
+        Task<IEnumerable<Screen>> GetAll();
+    }
+}
