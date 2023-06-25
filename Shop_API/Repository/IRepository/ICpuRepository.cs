5adf9af6b7c9f90e34ce2f842009a2f8f158b762
Phạm Quang Vinh <quangvinh770808@gmail.com>
2023-06-25 08:08:41 +0700

    complete cpu and role

---
+++ Shop_API/Repository/IRepository/ICpuRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shop_Models.Entities;
+
+namespace Shop_API.Repository.IRepository
+{
+    public interface ICpuRepository
+    {
+        Task<bool> Create(Cpu obj);
+        Task<bool> Update(Cpu obj);
+        Task<bool> Delete(Guid id);
+        Task<List<Cpu>> GetAllCpus();
+    }
+}
