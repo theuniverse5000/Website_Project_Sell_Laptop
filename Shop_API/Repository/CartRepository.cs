5e2fde91f76a9ab48523ddcf0495b939e1ca2cc6
minh <theuniverse5000@gmail.com>
2023-06-26 13:16:59 +0700

    Cart Repository

---
+++ Shop_API/Repository/CartRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop_API.AppDbContext;
+using Shop_API.Repository.IRepository;
+using Shop_Models.Entities;
+using Shop_Models.ViewModels;
+
+namespace Shop_API.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CartRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> Create(Cart obj)
+        {
+            var checkId = await _context.Carts.AnyAsync(x => x.UserId == obj.UserId);
+            if (obj == null || checkId == true)
+            {
+                return false;
+            }
+            try
+            {
+                await _context.Carts.AddAsync(obj);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public async Task<bool> Delete(Guid id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+            if (cart == null)
+            {
+                return false;
+            }
+            try
+            {
+                _context.Carts.Remove(cart);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<Cart>> GetAll()
+        {
+            return await _context.Carts.ToListAsync();
+        }
+
+        public async Task<Cart> GetById(Guid id)
+        {
+            var cart = await _context.Carts.FindAsync(id);
+            return cart;
+        }
+
+        public Task<IEnumerable<CartItem>> GetCartItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Update(Cart obj)
+        {
+            var cart = await _context.Carts.FindAsync(obj.UserId);
+            if (cart == null)
+            {
+                return false;
+            }
+            try
+            {
+                cart.Description = obj.Description;
+                _context.Carts.Update(cart);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Shop_API/Repository/IRepository/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using Shop_Models.Entities;
+using Shop_Models.ViewModels;
+
+namespace Shop_API.Repository.IRepository
+{
+    public interface ICartRepository
+    {
+        Task<bool> Create(Cart obj);
+        Task<bool> Update(Cart obj);
+        Task<bool> Delete(Guid id);
+        Task<IEnumerable<Cart>> GetAll();
+        Task<Cart> GetById(Guid id);
+        Task<IEnumerable<CartItem>> GetCartItem();
+    }
+}
