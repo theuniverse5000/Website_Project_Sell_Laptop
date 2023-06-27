3b2a0f0f7879ce1469f92a327c738af31a39f6e5
minh <theuniverse5000@gmail.com>
2023-06-28 00:01:58 +0700

    Add DI

--- Shop_API/Program.cs
+++ Shop_API/Program.cs
@@ -10,8 +10,16 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add DI 
 builder.Services.AddScoped<ApplicationDbContext, ApplicationDbContext>();
 builder.Services.AddTransient<IRamRepository, RamRepository>();
+builder.Services.AddTransient<ICardVGARepository, CardVGARepository>();
+builder.Services.AddTransient<ICpuRepository, CpuRepository>();
+builder.Services.AddTransient<IHardDriveRepository, HardDriveRepository>();
+builder.Services.AddTransient<ISanPhamGiamGiaRepository, SanPhamGiamGiaRepository>();
+builder.Services.AddTransient<IGiamGiaRepository, GiamGiaRepository>();
+builder.Services.AddTransient<IRoleRepository, RoleRepository>();
+builder.Services.AddTransient<ICpuRepository, CpuRepository>();
 builder.Services.AddTransient<IScreenRepository, ScreenRepository>();
 builder.Services.AddTransient<IProductDetailRepository, ProductDetailRepository>();
 builder.Services.AddTransient<IManufacturerRepository, ManufacturerRepository>();
@@ -23,6 +31,10 @@ builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IViDiemRepository, ViDiemRepository>();
 builder.Services.AddTransient<IQuyDoiDiemRepository, QuyDoiDiemRepository>();
 builder.Services.AddTransient<ILichSuTieuDiemRepository, LichSuTieuDiemRepository>();
+builder.Services.AddTransient<ICartRepository, CartRepository>();
+builder.Services.AddTransient<ICartDetailRepository, CartDetailRepository>();
+builder.Services.AddTransient<IBillRepository, BillRepository>();
+builder.Services.AddTransient<IBillDetailRepository, BillDetailRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
