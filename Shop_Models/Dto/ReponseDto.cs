a2738da1d9aa3625f23d3604dead36e62ff90902
minh <theuniverse5000@gmail.com>
2023-06-28 23:38:51 +0700

    Add DTO ( Data Transfer Objects )

---
+++ Shop_Models/Dto/ReponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop_Models.Dto
+{
+    public class ReponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "Thành công";
+    }
+}
