24d505bdef38f13e8a6c345fb8eb96273fbe72d0
minh <theuniverse5000@gmail.com>
2023-07-11 16:26:18 +0700

    Update CreateBill API

--- Shop_API/Controllers/BillController.cs
+++ Shop_API/Controllers/BillController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Shop_API.Repository.IRepository;
 using Shop_Models.Dto;
 using Shop_Models.Entities;
@@ -7,6 +8,7 @@ namespace Shop_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class BillController : ControllerBase
     {
         private readonly IBillRepository _billRepository;
@@ -30,6 +32,7 @@ namespace Shop_API.Controllers
             _reponse = new ReponseDto();
             _voucherRepository = voucherRepository;
         }
+        [Authorize]
         [HttpGet("GetAllBills")]
         public async Task<ReponseDto> GetAllBills()
         {
@@ -50,75 +53,134 @@ namespace Shop_API.Controllers
             }
 
         }
+        [AllowAnonymous]
         [HttpPost("CreateBill")]
         public async Task<ReponseDto> CreateBill(string username, string maVoucher)
         {
-            getUserId = _userRepository.GetAllUsers().Result.FirstOrDefault(x => x.Username == username).Id;
-            cartItem = await _cartRepository.GetCartItem(username);
-            if (maVoucher == null || cartItem == null)
+            try // Bắt lỗi ngoại lệ
             {
-                _reponse.Result = null;
-                _reponse.IsSuccess = false;
-                _reponse.Code = 404;
-                _reponse.Message = "Không có sản phẩm trong giỏ hàng";
-                return _reponse;
-            }
-            else
-            {
-                var listVoucher = await _voucherRepository.GetAllVouchers();
-                var voucherX = listVoucher.FirstOrDefault(x => x.MaVoucher == maVoucher);
-                Bill bill = new Bill()
-                {
-                    Id = Guid.NewGuid(),
-                    Ma = "Bill" + DateTime.Now.ToString(),
-                    CreateDate = DateTime.Now,
-                    HoTenKhachHang = "1",
-                    SDTKhachHang = "2",
-                    DiaChiKhachHang = "3",
-                    Status = 1,
-                    UserId = getUserId,
-                    VoucherId = voucherX != null ? voucherX.Id : null
-                };
-                if (await _billRepository.Create(bill))
-                {
-                    foreach (var x in cartItem)
-                    {
-                        List<BillDetail> listBillDetal = new List<BillDetail>();
-                        BillDetail billDetail = new BillDetail();
-                        billDetail.Id = Guid.NewGuid();
-                        billDetail.BillId = bill.Id;
-                        billDetail.ProductDetailId = x.IdProductDetails;
-                        billDetail.Quantity = x.Quantity;
-                        billDetail.Price = Convert.ToDouble(x.Price);
-                        if (!await _billDetailRepository.CreateBillDetail(billDetail))
-                        {
-                            _reponse.Result = null;
-                            _reponse.IsSuccess = false;
-                            _reponse.Code = 404;
-                            _reponse.Message = "Lỗi khi thêm sản phẩm vào hóa đơn";
-                            return _reponse;
-                        }
-                    }
-                    _reponse.Result = null;
-                    _reponse.IsSuccess = true;
-                    _reponse.Code = 200;
-                    _reponse.Message = "Thành công";
-                    return _reponse;
-                }
-                else
+                var user = _userRepository.GetAllUsers().Result.FirstOrDefault(x => x.Username == username);
+                if (user == null)
                 {
                     _reponse.Result = null;
                     _reponse.IsSuccess = false;
                     _reponse.Code = 404;
-                    _reponse.Message = "Không thể tạo hóa đơn";
-                    return _reponse;
+                    _reponse.Message = "Không tìm thấy tài khoản người dùng";
+                    return _reponse;// Nếu chưa có sản phẩm trong giỏ hàng thì không thể tạo hóa đơn
+                }
+                getUserId = user.Id;
+                cartItem = await _cartRepository.GetCartItem(username);// Tìm  ra giỏ hàng của user
+                if (cartItem == null)// Kiểm tra giỏ hàng của người dùng 
+                {
+                    _reponse.Result = null;
+                    _reponse.IsSuccess = false;
+                    _reponse.Code = 404;
+                    _reponse.Message = "Không có sản phẩm trong giỏ hàng";
+                    return _reponse;// Nếu chưa có sản phẩm trong giỏ hàng thì không thể tạo hóa đơn
+                }
+                else
+                {
+                    var listVoucher = await _voucherRepository.GetAllVouchers();// Lấy danh sách voucher
+                    var voucherX = listVoucher.FirstOrDefault(x => x.MaVoucher == maVoucher);// Kiểm tra xem có voucher ko
+                    if (voucherX == null)// Không có thì trả về kết quả
+                    {
+                        _reponse.Result = null;
+                        _reponse.IsSuccess = false;
+                        _reponse.Code = 404;
+                        _reponse.Message = "Voucher sai hoặc không tồn tại";
+                        return _reponse;
+                    }
+                    int checkSlVoucher = voucherX.SoLuong;
+                    DateTime startTimeVoucher = voucherX.StarDay;
+                    DateTime endTimeVoucher = voucherX.EndDay;
+                    if (checkSlVoucher <= 0)
+                    {
+                        _reponse.Result = null;
+                        _reponse.IsSuccess = false;
+                        _reponse.Code = 404;
+                        _reponse.Message = "Voucher đã hết lượt sử dụng";
+                        return _reponse;
+                    }
+                    if (startTimeVoucher > DateTime.Now || DateTime.Now > endTimeVoucher)
+                    {
+                        _reponse.Result = null;
+                        _reponse.IsSuccess = false;
+                        _reponse.Code = 404;
+                        _reponse.Message = "Voucher đã hết hạn sử dụng hoặc chưa đến ngày sử dụng";
+                        return _reponse;
+                    }
+                    Bill bill = new Bill()// Tạo hóa đơn
+                    {
+                        Id = Guid.NewGuid(),
+                        Ma = "Bill" + DateTime.Now.ToString(),
+                        CreateDate = Convert.ToDateTime(DateTime.Now.ToString()),
+                        HoTenKhachHang = user.FullName,
+                        SDTKhachHang = user.PhoneNumber,
+                        DiaChiKhachHang = user.Address,
+                        Status = 1,
+                        UserId = getUserId,
+                        VoucherId = voucherX != null ? voucherX.Id : null
+                    };
+                    if (await _billRepository.Create(bill))// Nếu tạo hóa đơn thành công thì tiếp tục 
+                    {
+                        foreach (var x in cartItem)
+                        {
+                            var productDetailX = _productDetailRepository.GetAll().Result.FirstOrDefault(x => x.Id == x.Id);
+                            if (productDetailX == null)
+                            {
+                                _reponse.IsSuccess = false;
+                                _reponse.Code = 404;
+                                _reponse.Message = "Sản phẩm không tồn tại";
+                                return _reponse;
+                            }
+                            int soLuongProductDetail = productDetailX.AvailableQuantity;
+                            if (soLuongProductDetail <= 0 || soLuongProductDetail < x.Quantity)
+                            {
+                                _reponse.Result = null;
+                                _reponse.IsSuccess = false;
+                                _reponse.Code = 404;
+                                _reponse.Message = "Số lượng sản phẩm trong kho không đủ";
+                                return _reponse;
+                            }
+                            BillDetail billDetail = new BillDetail();
+                            billDetail.Id = Guid.NewGuid();
+                            billDetail.BillId = bill.Id;
+                            billDetail.ProductDetailId = x.IdProductDetails;
+                            billDetail.Quantity = x.Quantity;
+                            billDetail.Price = Convert.ToDouble(x.Price);
+                            if (!await _billDetailRepository.CreateBillDetail(billDetail))
+                            {
+                                _reponse.Result = null;
+                                _reponse.IsSuccess = false;
+                                _reponse.Code = 404;
+                                _reponse.Message = "Lỗi khi thêm sản phẩm vào hóa đơn";
+                                return _reponse;
+                            }
+                        }
+                        _reponse.Result = null;
+                        _reponse.IsSuccess = true;
+                        _reponse.Code = 200;
+                        _reponse.Message = "Thành công";
+                        return _reponse;
+                    }
+                    else // Nếu không tạo được hóa đơn thì trả về kết quả
+                    {
+                        _reponse.Result = null;
+                        _reponse.IsSuccess = false;
+                        _reponse.Code = 404;
+                        _reponse.Message = "Không thể tạo hóa đơn";
+                        return _reponse;
+                    }
                 }
             }
-            _reponse.Result = null;
-            _reponse.IsSuccess = false;
-            _reponse.Code = 404;
-            _reponse.Message = "g";
-            return _reponse;
+            catch (Exception e)
+            {
+                _reponse.Result = null;
+                _reponse.IsSuccess = false;
+                _reponse.Code = 404;
+                _reponse.Message = "Có lỗi gì đó: " + e.Message;
+                return _reponse;
+            }
         }
     }
 }
