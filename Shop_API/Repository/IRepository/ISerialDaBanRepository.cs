8007f0298f8d11199cd7d27eccd02d193933a4b1
Nam Hai <hainnph27584@fpt.edu.vn>
2023-07-26 01:37:50 +0700

    ISerialDaBanRepository and SerialDaBanRepository

---
+++ Shop_API/Repository/IRepository/ISerialDaBanRepository.cs
@@ -0,0 +1,13 @@
+﻿using Shop_Models.Entities;
+
+namespace Shop_API.Repository.IRepository
+{
+    public interface ISerialDaBanRepository
+    {
+        Task<bool> Create(SerialDaBan obj);
+        Task<bool> Update(SerialDaBan obj);
+        Task<bool> Delete(Guid id);
+        Task<IEnumerable<SerialDaBan>> GetAll();
+
+    }
+}
