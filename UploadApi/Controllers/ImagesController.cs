e54f241616c8bac446962f5fa5e7b70485db6db0
Nam Hai <hainnph27584@fpt.edu.vn>
2023-09-05 09:47:33 +0700

    add ImagesController in UploadAPI

---
+++ UploadApi/Controllers/ImagesController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop_Models.Entities;
+using UploadApi.Services.IServices;
+
+namespace UploadApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImagesServies _imageUploadService;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public ImagesController(IImagesServies imageUploadService, IWebHostEnvironment hostingEnvironment)
+        {
+            _imageUploadService = imageUploadService;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+
+
+        [HttpPost]
+        [Route("anh")]
+        public async Task<IActionResult> taianh(IFormFile file, string objectType, Guid? objectId, string imageCode)
+        {
+            if (file == null)
+            {
+                return BadRequest("Vui lòng chọn một tệp để tải lên và lưu.");
+            }
+
+            string objectFolder = null;
+            bool saveToDb = false;
+
+            switch (objectType)
+            {
+                case "a":
+                    objectFolder = "product_images";
+                    saveToDb = true;
+                    break;
+                case "b":
+                    objectFolder = "user_images";
+                    saveToDb = false;
+                    break;
+                case "c":
+                    objectFolder = "bai_dang_images";
+                    saveToDb = true;
+                    break;
+                default:
+                    return BadRequest("Loại đối tượng không hợp lệ.");
+            }
+
+            string basePath = _hostingEnvironment.WebRootPath; // Đường dẫn gốc của thư mục wwwroot
+            string fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+            string filePath = Path.Combine(basePath, objectFolder, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            if (saveToDb)
+            {
+                var image = new Image
+                {
+                    Id = Guid.NewGuid(),
+                    Ma = imageCode, // Mã của ảnh
+                    LinkImage = $"/{objectFolder}/{fileName}", // Đường dẫn tương đối đến ảnh
+                    ProductDetailId = (Guid)objectId, // Đối tượng ProductDetail
+                    Status = 1 // Trạng thái ảnh
+                };
+
+                await _imageUploadService.SaveImageAsync(image);
+            }
+            if (objectType == "c")
+            {
+
+            }
+
+            return Ok("Tải lên và lưu thành công.");
+        }
+
+
+
+    }
+}
--- UploadApi/Program.cs
+++ UploadApi/Program.cs
@@ -1,4 +1,13 @@
+using UploadApi.Services.IServices;
+using UploadApi.Services;
+using Shop_API.AppDbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 // Add services to the container.
 
@@ -6,7 +15,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ApplicationDbContext, ApplicationDbContext>();
 
+builder.Services.AddTransient<IImagesServies, ImagesServies>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
