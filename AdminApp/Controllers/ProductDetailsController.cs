f80c9547914c4d867581fcef0db6af1b688def0c
Nam Hai <hainnph27584@fpt.edu.vn>
2023-09-13 16:18:50 +0700

    add controller: acction Index, AllProductDetails

---
+++ AdminApp/Controllers/ProductDetailsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminApp.Controllers
+{
+	public class ProductDetailsController : Controller
+	{
+        private readonly ILogger<ProductDetailsController> _logger;
+
+        public ProductDetailsController(ILogger<ProductDetailsController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult AllProductDetails()
+		{
+			return View();
+		}
+	}
+}
