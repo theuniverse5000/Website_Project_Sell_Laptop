0a520938205e45f932ee888771efbd8aecc08fa1
minh <theuniverse5000@gmail.com>
2023-09-16 11:39:19 +0700

    Utilities (Tiện ích cho shop api)

---
+++ Shop_API/Utilities/SendEmail.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using Shop_Models.Dto;
+
+namespace Shop_API.Utilities
+{
+    public class SendEmail
+    {
+        private readonly IConfiguration _config;
+
+        public SendEmail(IConfiguration config)
+        {
+            _config = config;
+        }
+        public bool SendEmailTo(EmailDto request)
+        {
+            try
+            {
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
+                email.To.Add(MailboxAddress.Parse(request.To));
+                email.Subject = request.Subject;
+                email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+                using var smtp = new SmtpClient();
+                smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+                smtp.Authenticate(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+                smtp.Send(email);
+                smtp.Disconnect(true);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
