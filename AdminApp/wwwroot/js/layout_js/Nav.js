67cb956d886d525b7450f87d42e0fe3728c4eb16
Phạm Quang Vinh <quangvinh770808@gmail.com>
2023-09-18 01:26:26 +0700

    complete layout Admin

---
+++ AdminApp/wwwroot/js/layout_js/Nav.js
@@ -0,0 +1,16 @@
+﻿
+var thongKe = document.getElementById('thongKe');
+thongKe.addEventListener('click', function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: "/Statistics/GetAllBills",
+        type: "GET",
+        success: function (result) {
+            $("#root").html(result);
+        },
+        error: function () {
+            alert("Có lỗi xảy ra.");
+        }
+    });
+
+})
