1d09b96cca9795d74378ddfda2c2ac2f3efa3209
Hoang Quan <quannhph27585@fpt.edu.vn>
2023-09-19 13:50:54 +0700

    account va login

---
+++ Shop_API/Service/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using Shop_API.Service.IService;
+using Shop_Models.Dto;
+using Shop_Models.Dto.Account;
+
+namespace Shop_API.Service
+{
+    public class AccountService : IAccountService
+    {
+        public Task<LoginResponesDto> RenewToken(TokenDto tokenDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SignUp(SignUpDto p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponesDto> Validate(LoginRequestDto loginRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
