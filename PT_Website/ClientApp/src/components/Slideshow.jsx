099d42fdf31b2e483848794f98fac0652e98c822
minh <theuniverse5000@gmail.com>
2023-10-10 19:00:21 +0700

    Web use React

---
+++ PT_Website/ClientApp/src/components/Slideshow.jsx
@@ -0,0 +1,37 @@
+﻿import React, { useState } from 'react';
+import './Slideshow.css';
+
+const Slideshow = ({ images }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
+    return (
+        <div className="slideshow-container">
+            <button className="prev" onClick={prevSlide}>
+                &#10094;
+            </button>
+            {images.map((image, index) => (
+                <div
+                    key={index}
+                    className={`slide ${index === currentIndex ? 'active' : ''}`}
+                >
+                    <img src={image} alt={`Slide ${index}`} />
+                </div>
+            ))}
+            <button className="next" onClick={nextSlide}>
+                &#10095;
+            </button>
+        </div>
+    );
+};
+
+export default Slideshow;
