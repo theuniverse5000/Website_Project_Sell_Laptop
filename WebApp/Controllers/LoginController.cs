788bd3979cd4fc8b2df18d6a8af4c6bf004aabff
Hoang Quan <quannhph27585@fpt.edu.vn>
2023-10-30 11:46:45 +0700

    fix login

--- WebApp/Controllers/LoginController.cs
+++ WebApp/Controllers/LoginController.cs
@@ -9,6 +9,10 @@ using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using Azure.Core;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.DotNet.MSIdentity.Shared;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authentication.Facebook;
 
 namespace WebApp.Controllers;
 [AllowAnonymous]
@@ -21,27 +25,27 @@ public class LoginController : Controller
         _httpClientFactory = httpClientFactory;
         _httpContextAccessor = httpContextAccessor;
     }
-   
+
     public IActionResult Login(string ReturnUrl = "/")
     {
         string accessToken = HttpContext.Request.Cookies["access_token"];
-        if(accessToken != null)
+        if (accessToken != null)
         {
             return Content(accessToken);
         }
 
         return PartialView("_Login");
-    }                                                                                                                                                                                                                               
+    }
     [HttpPost]
-    public async Task<IActionResult> LoginWithJWT([FromBody]LoginRequestDto  loginRequestDto)
+    public async Task<IActionResult> LoginWithJWT([FromBody] LoginRequestDto loginRequestDto)
     {
         var apiUrl = "https://localhost:7286/api/Account/Login";
         var httpclient = _httpClientFactory.CreateClient("MyHttpClient");
-        var requestdata= new StringContent(JsonConvert.SerializeObject(loginRequestDto),Encoding.UTF8, "application/json");
+        var requestdata = new StringContent(JsonConvert.SerializeObject(loginRequestDto), Encoding.UTF8, "application/json");
         var respone = await httpclient.PostAsync(apiUrl, requestdata);
         //var LoginRespones = JsonConvert.DeserializeObject<LoginResponesDto>(respone.Content);
         var jsonRespone = await respone.Content.ReadAsStringAsync();
-        var LoginRespones= JsonConvert.DeserializeObject<LoginResponesDto>(jsonRespone);
+        var LoginRespones = JsonConvert.DeserializeObject<LoginResponesDto>(jsonRespone);
         // Lưu trữ access token trong một cookie
         HttpContext.Response.Cookies.Append("access_token", LoginRespones.Data.ToString(), new CookieOptions
         {
@@ -56,5 +60,76 @@ public class LoginController : Controller
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         //Redirect to home page
         return LocalRedirect("/");
+    }
+    public IActionResult LoginWithGoogle(string returnUrl = "/")
+    {
+        var properties = new AuthenticationProperties
+        {
+            RedirectUri = Url.Action(nameof(GoogleLoginCallback), new { returnUrl })
+        };
+
+        return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+    }
+
+    public async Task<IActionResult> GoogleLoginCallback(string returnUrl = "/")
+    {
+        var authenticateResult = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        if (authenticateResult.Succeeded)
+        {
+
+
+            var email = authenticateResult.Principal.FindFirstValue(ClaimTypes.Email);
+            var name = authenticateResult.Principal.FindFirstValue(ClaimTypes.Name);
+
+            // Lưu thông tin đăng nhập vào cookie
+            HttpContext.Response.Cookies.Append("access_token", "YOUR_ACCESS_TOKEN", new CookieOptions
+            {
+                Expires = DateTime.Now.AddHours(3),
+            });
+
+            return LocalRedirect(returnUrl);
+        }
+        else
+        {
+
+
+            return RedirectToAction("Login");
+        }
+    }
+
+
+    public IActionResult LoginWithFacebook(string returnUrl = "/")
+    {
+        var properties = new AuthenticationProperties
+        {
+            RedirectUri = Url.Action(nameof(FacebookLoginCallback), new { returnUrl })
+        };
+
+        return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+    }
+
+    public async Task<IActionResult> FacebookLoginCallback(string returnUrl = "/")
+    {
+        var authenticateResult = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        if (authenticateResult.Succeeded)
+        {
+
+            var email = authenticateResult.Principal.FindFirstValue(ClaimTypes.Email);
+            var name = authenticateResult.Principal.FindFirstValue(ClaimTypes.Name);
+
+            HttpContext.Response.Cookies.Append("access_token", "YOUR_ACCESS_TOKEN", new CookieOptions
+            {
+                Expires = DateTime.Now.AddHours(3),
+            });
+
+            return LocalRedirect(returnUrl);
+        }
+        else
+        {
+
+            return RedirectToAction("Login");
+        }
     }
 }
