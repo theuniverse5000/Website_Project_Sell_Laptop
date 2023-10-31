820b406337894cc27257cdd065e2531e5c8eb0a5
ADMIN <ADMIN@LAPTOP-IHE70EQ6>
2023-10-31 08:29:13 +0700

    them open Product Detail

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,21 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Shop_Models.Dto;
 
 namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
         {
-            var apiUrl = "{{baseUrl}}/api/ProductDetail/GetAllNoJoin";
+            _httpClientFactory=httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = _httpClientFactory.CreateClient("PhuongThaoHttpWeb");
+            var apiUrl = "/api/ProductDetail/PGetProductDetail";
+            var apiRespone = await httpClient.GetStringAsync(apiUrl);
+            var Respone = apiRespone.ToString();
+            var responeModel = JsonConvert.DeserializeObject<ResponseDto>(Respone);
+            var content = JsonConvert.DeserializeObject<List<ProductDetailDto>>(responeModel.Result.ToString());
+            ViewBag.ProductDetails = content;
             return View();
         }
         public IActionResult GoLogin()
         {
             return RedirectToAction("Login","Login");
         }
-        public IActionResult ProductDetail()
+        
+        public async Task<IActionResult> ProductDetail(Guid id)
         {
-            return PartialView("_Detail");
+            var httpClient = _httpClientFactory.CreateClient("PhuongThaoHttpWeb");
+            var apiUrl = $@"/api/ProductDetail/ProductDetailById?guid={id}";
+            var apiRespone = await httpClient.GetStringAsync(apiUrl);
+            var content = JsonConvert.DeserializeObject<ProductDto>(apiRespone);
+            ViewBag.ProductDetail = content;
+            return PartialView("_Detail",content);
         }
     }
 }
--- WebApp/wwwroot/js/Product.js
+++ WebApp/wwwroot/js/Product.js
@@ -4,8 +4,12 @@ var hotProducts = document.getElementsByClassName('hot-product');
 for (var i = 0; i < hotProducts.length; i++) {
     hotProducts[i].addEventListener('click', function (event) {
         event.preventDefault()
+        var id = this.getAttribute('data-id');
+
+        
+        
         $.ajax({
-            url: "/Home/ProductDetail",
+            url: "/Home/ProductDetail/" + id,
             type: "GET",
             success: function (result) {
                 $("#root").html(result); // Cập nhật nội dung của phần tử trên trang web
