8f7a054b710c32c822e7a930cfdc6c46a5436654
minh <theuniverse5000@gmail.com>
2023-11-16 16:39:35 +0700

    Fix error db

--- Shop_Models/Entities/User.cs
+++ Shop_Models/Entities/User.cs
@@ -11,6 +11,8 @@ namespace Shop_Models.Entities
         public string? FullName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
+        public virtual ViDiem? ViDiem { get; set; }
+        public virtual Cart? Cart { get; set; }
         public Guid RoleId { get; set; }
     }
 }
