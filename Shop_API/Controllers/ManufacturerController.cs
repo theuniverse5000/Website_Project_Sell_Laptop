5143ed470f9df6a1b73753aba80610f494b6b05b
minh <theuniverse5000@gmail.com>
2023-11-23 23:31:58 +0700

    Auth

--- Shop_API/Controllers/ManufacturerController.cs
+++ Shop_API/Controllers/ManufacturerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Shop_API.Repository.IRepository;
 using Shop_Models.Dto;
 using Shop_Models.Entities;
