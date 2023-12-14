3371a5417c5999168d3aa34f6840ca82c27f7120
Nam Hai <hainnph27584@fpt.edu.vn>
2023-12-14 22:50:54 +0700

    thong ke

--- Shop_API/Controllers/BillController.cs
+++ Shop_API/Controllers/BillController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Serilog;
+using Shop_API.AppDbContext;
 using Shop_API.Repository.IRepository;
 using Shop_API.Service.IService;
+using Shop_Models.Dto;
 using Shop_Models.Entities;
 
 namespace Shop_API.Controllers
@@ -15,12 +17,14 @@ namespace Shop_API.Controllers
         private readonly IBillService _billService;
         private readonly IBillRepository _billRepository;
         private readonly IConfiguration _config;
+        private readonly ApplicationDbContext _context;
         public BillController(IConfiguration config,
-            IBillService billService, IBillRepository billRepository)
+            IBillService billService, IBillRepository billRepository, ApplicationDbContext context)
         {
             _config = config;
             _billService = billService;
             _billRepository = billRepository;
+            _context = context;
         }
         [AllowAnonymous]
         [HttpGet("PGetBillByInvoiceCode")]
@@ -83,5 +87,116 @@ namespace Shop_API.Controllers
             }
             return BadRequest("Cập nhật thất bại");
         }
+
+
+
+
+        //[AllowAnonymous]
+        //[HttpGet("GetRevenueStatistics")]
+        //public IActionResult GetRevenueStatistics()
+        //{
+        //    var revenueData = _context.BillDetails
+        //        .GroupBy(bd => new { Year = bd.Bill.CreateDate.Year, Month = bd.Bill.CreateDate.Month })
+        //        .Select(g => new RevenueDto
+        //        {
+        //            Date = new DateTime(g.Key.Year, g.Key.Month, 1),
+        //            Amount = (decimal)g.Sum(bd => bd.Quantity * bd.Price)
+        //        })
+        //        .ToList();
+
+        //    return Ok(revenueData);
+        //}
+
+        [AllowAnonymous]
+        [HttpGet("GetRevenueStatistics")]
+        public IActionResult GetRevenueStatistics(int year)
+        {
+            var revenueData = _context.BillDetails
+                .Where(bd => bd.Bill.CreateDate.Year == year)
+                .GroupBy(bd => new { Month = bd.Bill.CreateDate.Month })
+                .Select(g => new RevenueDto
+                {
+                    Date = new DateTime(year, g.Key.Month, 1),
+                    Amount = (decimal)g.Sum(bd => bd.Quantity * bd.Price)
+                })
+                .ToList();
+
+            return Ok(revenueData);
+        }
+
+
+        [AllowAnonymous]
+        [HttpGet("GetRevenueStatisticss")]
+        public IActionResult GetRevenueStatisticss(DateTime selectedDate)
+        {     
+                var startDate = selectedDate.Date;
+                var endDate = startDate.AddMonths(1).AddDays(-1);
+
+                var dailyStatistics = _context.Bills
+                    .Where(b => b.CreateDate >= startDate && b.CreateDate <= endDate)
+                    .Join(
+                        _context.BillDetails,
+                        bill => bill.Id,
+                        billDetail => billDetail.BillId,
+                        (bill, billDetail) => new
+                        {
+                            Day = bill.CreateDate.Day,
+                            Amount = billDetail.Price * billDetail.Quantity,
+                            // Add other fields as needed
+                        }
+                    )
+                    .GroupBy(result => result.Day)
+                    .Select(group => new
+                    {
+                        Day = group.Key,
+                        Amount = group.Sum(result => result.Amount),
+                        TotalOrders = group.Count(),
+                        // Add other fields as needed
+                    })
+                    .ToList();
+                return Ok(dailyStatistics);
+        }
+
+
+
+        [AllowAnonymous]
+        [HttpGet("test")]
+        public IActionResult test(int? year, int? month)
+        {
+            var startDate = year.HasValue && month.HasValue
+                ? new DateTime(year.Value, month.Value, 1)
+                : DateTime.MinValue;
+
+            var endDate = startDate != DateTime.MinValue
+                ? startDate.AddMonths(1).AddDays(-1)
+                : DateTime.MaxValue;
+
+            var statistics = _context.Bills
+                .Where(b => b.CreateDate >= startDate && b.CreateDate <= endDate)
+                .Join(
+                    _context.BillDetails,
+                    bill => bill.Id,
+                    billDetail => billDetail.BillId,
+                    (bill, billDetail) => new
+                    {
+                        Date = bill.CreateDate,
+                        Amount = billDetail.Price * billDetail.Quantity,
+                        // Add other fields as needed
+                    }
+                )
+                .GroupBy(result => result.Date)
+                .Select(group => new
+                {
+                    Date = group.Key,
+                    Amount = group.Sum(result => result.Amount),
+                    TotalOrders = group.Count(),
+                    // Add other fields as needed
+                })
+                .ToList();
+
+            return Ok(statistics);
+        }
+
+
     }
 }
