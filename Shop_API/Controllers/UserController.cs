a896f0534b711896df90f2ab28ba3135ae07deee
Hoang Quan <quannhph27585@fpt.edu.vn>
2023-12-18 23:32:48 +0700

    api sua thong tin, api doi mat khau

--- Shop_API/Controllers/UserController.cs
+++ Shop_API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Azure;
 using MailKit;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Protocol.Core.Types;
@@ -135,6 +136,61 @@ namespace Shop_API.Controllers
             _reponse.Result = _iPagingRepository.GetAllUser(search, from, to, sortBy, page);
             var count = _reponse.Count = _iPagingRepository.GetAllUser(search, from, to, sortBy, page).Count;
             return Ok(_reponse);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> ChangeContactInfo([FromBody] ChangeContactInfoDto dto)
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            // Update address if provided
+            if (!string.IsNullOrEmpty(dto.NewAddress))
+            {
+                currentUser.Address = dto.NewAddress;
+            }
+
+            // Update phone number if provided
+            if (!string.IsNullOrEmpty(dto.NewPhoneNumber))
+            {
+                currentUser.PhoneNumber = dto.NewPhoneNumber;
+            }
+
+            // Save changes
+            var result = await _userManager.UpdateAsync(currentUser);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(new { Message = "Contact information changed successfully" });
+        }
+
+        [HttpPost]
+        [Authorize] // Assuming only authenticated users can change their password
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordDto dto)
+        {
+            // Get the current user
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            // Verify the old password
+            var passwordVerificationResult = await _userManager.CheckPasswordAsync(currentUser, dto.OldPassword);
+
+            if (!passwordVerificationResult)
+            {
+                return BadRequest("Invalid old password");
+            }
+
+            // Change the password
+            var result = await _userManager.ChangePasswordAsync(currentUser, dto.OldPassword, dto.NewPassword);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(new { Message = "Password changed successfully" });
         }
 
 
---
+++ Shop_Models/Dto/ChangeContactInfoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Models.Dto
+{
+    public class ChangeContactInfoDto
+    {
+        public string NewAddress { get; set; }
+        public string NewPhoneNumber { get; set; }
+    }
+}
